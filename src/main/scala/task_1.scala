9478e9e004050764ffc22a05c88e4e8d33ccdc15
Even Grøtte <eogrotte@stud.ntnu.no>
2018-02-27 08:59:35 +0100

    Completed task 1 (but have not written to file yet), did 1j) and 1k)

--- src/main/scala/task_1.scala
+++ src/main/scala/task_1.scala
@@ -16,7 +16,7 @@ object task_1 {
 
     // load tweets
     var geotweets = sc.textFile("data/geotweets.tsv")
-    geotweets = geotweets.sample(false, 0.0001, 5)
+    //geotweets = geotweets.sample(false, 0.0001, 5)
     //geotweets.saveAsTextFile(".\\testdata.txt")
 
     // a) How many tweets are there?
@@ -25,7 +25,7 @@ object task_1 {
     printf("tweetCount: %d\n", tweetCount, "\n")
 
     // b) How many distinct users (username) are there?
-    val users = geotweets.map(line => line.split("\t")(6))
+    val users = geotweets.map(line => line.split("\t")(6)) //Looking at the file, it seems that usernames are at 7, while user screen names are at 6.
     val uniqueUsers = users.distinct()
     val numOfUniqueUsers = uniqueUsers.count()
 
@@ -47,8 +47,6 @@ object task_1 {
 
     printf("Number of unique places: %d\n", numOfUniquePlaces, "\n")
 
-    //places.foreach(println)
-
     // e) In how many languages users post tweets?
 
     val languages = geotweets.map(line => line.split("\t")(5))
@@ -58,7 +56,7 @@ object task_1 {
     printf("Number of unique languages: %d\n", numOfUniqueLanguages, "\n")
 
     // f) What is the minimum latitude?
-    val latitude = geotweets.map(line => line.split("\t")(11))
+    val latitude = geotweets.map(line => line.split("\t")(11).toDouble)
     val minimumLatitude = latitude.min()
 
     printf("Minimum latitude: ")
@@ -66,7 +64,7 @@ object task_1 {
 
 
     // g) What is the minimum longitude?
-    val longitude = geotweets.map(line => line.split("\t")(12))
+    val longitude = geotweets.map(line => line.split("\t")(12).toDouble)
     val minimumLongitude = longitude.min()
     printf("Minimum longitude: ")
     println(minimumLongitude)
@@ -84,8 +82,23 @@ object task_1 {
 
     // j) What is the average length of a tweet text in terms of characters?
 
-    val tweetText = geotweets.map(line => line.split("\t")(10))
-    
+    val tweetTextLength = geotweets.map(line => line.split("\t")(10).length.toLong)
+    var i = 0;
+    var totalChars = 0;
+    val totalCommentLength = tweetTextLength.reduce((a,b)=>(a + b))
+    val averageCommentLength = totalCommentLength/tweetCount
+
+    printf("Average length of tweet text: ")
+    println(averageCommentLength)
+
+    // k) What is the average length of a tweet text in terms of words?
+    val tweetTexts = geotweets.map(line => line.split("\t")(10))
+    val tweetWords = tweetTexts.flatMap(line => line.split(" ")) //Inclues stuff like "&amp;"
+    val amountOfWords = tweetWords.count()
+    val averageWordCount = amountOfWords/tweetCount
+
+    printf("Average words per tweet: ")
+    println(averageWordCount)
   }
 
 }
