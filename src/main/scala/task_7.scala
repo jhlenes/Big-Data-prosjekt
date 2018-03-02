d7dee6660a53caf16d2eb59550f2de93e623d9f3
Jan Henrik Lenes <jhlenes@outlook.com>
2018-03-02 15:23:05 +0100

    Complete task 7

--- src/main/scala/task_7.scala
+++ src/main/scala/task_7.scala
@@ -1,11 +1,11 @@
 object task_7 {
 
-  import org.apache.spark.SparkContext
-  import org.apache.spark.SparkConf
-  import org.apache.log4j.Logger
-  import org.apache.log4j.Level
+  import org.apache.log4j.{Level, Logger}
+  import org.apache.spark.{SparkConf, SparkContext}
 
   def main(args: Array[String]): Unit = {
+    val t0 = System.nanoTime()
+
     // disable logging
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -15,68 +15,72 @@ object task_7 {
     val sc = new SparkContext(conf)
 
     // set result file
-    val resultFile = "result_5.tsv"
+    val resultFile = "result_7.tsv"
     val resultDirectory = resultFile.replace(".tsv", "")
     ResultManager.deletePreviousResult(resultDirectory)
 
-    /*   Task 7: Find the 5 cities in the US with the highest number of tweets (place_type = ’city’ and
+    /* Task 7: Find the 5 cities in the US with the highest number of tweets (place_type = ’city’ and
         country_code = ’US’, ordered by their tweet counts/alphabetical). For these 5 cities,
         find the 10 most frequent words ordered by their frequency, ignoring the stop words
-          from the file and excluding words shorted than 2 characters (length < 2). Write a code
+        from the file and excluding words shorted than 2 characters (length < 2). Write a code
         (named “task_7”) that writes the results in a TSV file named “result_7.tsv” in the
-          form of <place_name>tab<word1>tab<frequency1>tab<word2>tab<frequency2> ...
-          <word10>tab<frequency10>.*/
+        form of <place_name>tab<word1>tab<frequency1>tab<word2>tab<frequency2> ...
+        <word10>tab<frequency10>.*/
 
-    def makeTuple(line: String): (((String, String, String, String))) = {
+    def makeTuple(line: String): (String, String, String) = {
       val splitted = line.split("\t")
       val countryCode = splitted(2)
       val placeType = splitted(3)
       val placeName = splitted(4)
+      (countryCode, placeType, placeName)
+    }
+
+    def makeTuple2(line: String): (String, String) = {
+      val splitted = line.split("\t")
+      val placeName = splitted(4)
       val tweetText = splitted(10)
-      (countryCode, placeType, placeName, tweetText)
+      (placeName, tweetText)
+    }
+
+    def toTsv(tuple: (String, List[(String, Int)])): String = {
+      val res = new StringBuilder(tuple._1)
+      tuple._2.foreach({ case (word, count) => res.append("\t" + word + "\t" + count) })
+      res.toString()
     }
 
     var geotweets = sc.textFile("data/geotweets.tsv")
-    val stopWords = sc.textFile("data/stop_words.txt")
+    //geotweets = geotweets.sample(false, 0.01)
+    val stopWords = sc.textFile("data/stop_words.txt").map((_, 1))
 
 
-    val res = geotweets.map(makeTuple) //countryCode, place type, place name, tweet text
-      .filter({ case (countryCode, placeType, _, _) => countryCode == "US" && placeType == "city" })
-      .map({ case (countryCode, placeType, placeName, tweetText) => (placeName, tweetText) })
-      .groupByKey()
-      .map({ case (placeName, tweetTexts) => (placeName, tweetTexts, tweetTexts.count(_ => true)) })
-      .sortBy(tuple => (-tuple._3, tuple._1))
-      .zipWithIndex
-      .filter { case (_, index) => index < 5 }
-      .keys
-      .map(tuple => (tuple._1, tuple._2))
-      .flatMapValues(tweetTexts => tweetTexts.flatMap(line => line.split(" ")))
-      .filter({ case (_, word) => word.length >= 2 })
-      .map(_.swap)
-      //.subtractByKey(stopWords)
+    val topFiveCities = geotweets.map(makeTuple)
+      .filter({ case (countryCode, placeType, _) => countryCode == "US" && placeType == "city" }) // keep only US cities
+      .map({ case (_, _, placeName) => (placeName, 1) })
+      .reduceByKey(_ + _)
+      .sortBy(tuple => (-tuple._2, tuple._1)) // descending count, alphabetical name
+      .map(tuple => tuple._1)
+      .collect().take(5)
 
+    val t2 = System.nanoTime()
 
-    //      .map({ case (city, word) => ((city, word), 1) })
-    //      .reduceByKey(_ + _)
-    //
-    //
-    //      .foreach(println)
+    println("Time used: " + (t2 - t0) + " ns")
 
-    //      .map(tuple => (tuple._1, tuple._2.flatMap(tweet => tweet.split(" ")))) // (country, [words])
-    //      .foreach(println)
+    val res = geotweets.map(makeTuple2)
+      .filter(tuple => topFiveCities.contains(tuple._1)) // keep only top five cities
+      .flatMapValues(tweetText => tweetText.split(" ")) // get the words
+      .filter({ case (_, word) => word.length >= 2 }) // remove short words
+      .map({ case (place, word) => (word.toLowerCase, place) }).subtractByKey(stopWords).map(_.swap) // remove stop words
+      .map({ case (place, word) => ((place, word), 1) }).reduceByKey(_ + _) // count the words
+      .map({ case ((place, word), count) => (place, (word, count)) }) // restructure KV pair
+      .mapValues(List(_)).reduceByKey(_ ++ _) // combine (word, count) KV pairs in a list
+      .mapValues(_.sortBy({ case (word, count) => (-count, word) }).take(10)) // take top 10 words
+      .map(toTsv)
 
-    //
-    //      .filter(_.length >= 2) // remove words with less than 2 characters
-    //      .map(_.toLowerCase)
-    //      .subtract(stopWords
+    res.coalesce(1).saveAsTextFile(resultDirectory) // save to file
+    ResultManager.moveResult(resultDirectory) // move results to a .tsv file
 
-
-    /*
-    .flatMap({ case (_, tweetText) => tweetText.split(" ") }) // get all the words
-    .filter(_.length >= 2) // remove words with less than 2 characters
-    .map(_.toLowerCase)
-    .subtract(stopWords)
-*/
+    val t1 = System.nanoTime()
+    println("Time used: " + (t1 - t0) + " ns")
 
   }
 
